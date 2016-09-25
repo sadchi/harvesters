f0ea05bcfda85fb5292aa78d33f9ddefef175b35
Sadchi <sadchi@gmail.com>
2016-09-26 00:15:11 +0300

    ref

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject harvesters "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.lwjgl/lwjgl "3.0.0"]]
+
+  :java-opts ["-Dorg.lwjgl.util.Debug=true" "-Dorg.lwjgl.librarypath=native"]
+
   :main ^:skip-aot harvesters.core
-  :target-path "target/%s"
+  :resource-paths ["resources" "native"]
+  :source-path "src"
   :profiles {:uberjar {:aot :all}})
