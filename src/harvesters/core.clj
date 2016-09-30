43e0843efae3298c363b45030b428d252710aed1
Sadchi <sadchi@gmail.com>
2016-09-30 22:55:40 +0300

    ref

--- src/harvesters/core.clj
+++ src/harvesters/core.clj
@@ -10,16 +10,7 @@
 
 (def window-title "Harvesters")
 
-(defonce globals (atom {:errorCallback nil
-                        :keyCallback   nil
-                        :window        nil
-                        :width         0
-                        :height        0
-                        :title         "none"
-                        :angle         0.0
-                        :last-time     0}))
-
-(defn- mk-window [{:keys [width height maximized] :or {width 800 height 600}}]
+(defn- init-window [{:keys [width height maximized] :or {width 800 height 600}}]
   (log/debug "Trying to create window using params:" [width height maximized])
   (GLFW/glfwSetErrorCallback (GLFWErrorCallback/createPrint System/err))
   (when-not (GLFW/glfwInit)
@@ -29,21 +20,19 @@
   (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
   (when maximized (GLFW/glfwWindowHint GLFW/GLFW_MAXIMIZED GLFW/GLFW_TRUE))
   (let [window (or (GLFW/glfwCreateWindow width height window-title 0 0) (throw (RuntimeException. "Failed to create the GLFW window")))
-        [res-width res-height] (if-not maximized [width height]
-                                                 (let [width-buf (int-array 1)
-                                                       height-buf (int-array 1)]
-                                                   (GLFW/glfwGetWindowSize window width-buf height-buf)
-                                                   [(aget width-buf 0) (aget height-buf 0)]))]
-    (GLFW/glfwMakeContextCurrent window)
-    (GLFW/glfwSwapInterval 1)
-    (GLFW/glfwShowWindow window)
-    {:window window
-     :width  res-width
-     :height res-height}))
+        width-buf (int-array 1)
+        height-buf (int-array 1)]
+        (GLFW/glfwMakeContextCurrent window)
+        (GLFW/glfwSwapInterval 1)
+        (GLFW/glfwShowWindow window)
+        (GLFW/glfwGetFramebufferSize window width-buf height-buf)
+        {:window              window
+         :frame-buffer-width  (aget width-buf 0)
+         :frame-buffer-height (aget height-buf 0)}))
 
 (defn init-gl [width height]
   (GL/createCapabilities)
-  (println "OpenGL version:" (GL11/glGetString GL11/GL_VERSION))
+  (log/debug "OpenGL version:" (GL11/glGetString GL11/GL_VERSION))
   (GL11/glClearColor 0.0 0.0 0.0 0.0)
   (GL11/glMatrixMode GL11/GL_PROJECTION)
   (GL11/glOrtho 0.0 width
@@ -51,51 +40,33 @@
                 -1.0 1.0)
   (GL11/glMatrixMode GL11/GL_MODELVIEW))
 
-(defn draw [width height]
-  (let [{:keys [angle]} @globals
-        w2 (/ width 2.0)
-        h2 (/ height 2.0)]
-    (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
-    (GL11/glLoadIdentity)
-    (GL11/glTranslatef w2 h2 0)
-    (GL11/glRotatef angle 0 0 1)
-    (GL11/glScalef 2 2 1)
-    (GL11/glBegin GL11/GL_TRIANGLES)
-    (do
-      (GL11/glColor3f 1.0 0.0 0.0)
-      (GL11/glVertex2i 100 0)
-      (GL11/glColor3f 0.0 1.0 0.0)
-      (GL11/glVertex2i -50 86.6)
-      (GL11/glColor3f 0.0 0.0 1.0)
-      (GL11/glVertex2i -50 -86.6))
-    (GL11/glEnd)))
+(defn draw []
 
-(defn update-globals []
-  (let [{:keys [angle last-time]} @globals
-        cur-time (System/currentTimeMillis)
-        delta-time (- cur-time last-time)
-        next-angle (+ (* delta-time 0.00005) angle)
-        next-angle (if (>= next-angle 360.0)
-                     (- next-angle 360.0)
-                     next-angle)]
-    (swap! globals assoc
-           :angle next-angle
-           :last-time cur-time)))
+  (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+  (GL11/glLoadIdentity)
+  (GL11/glBegin GL11/GL_TRIANGLES)
+  (do
+    (GL11/glColor3f 1.0 0.0 0.0)
+    (GL11/glVertex2f 0 0.5)
+    (GL11/glColor3f 0.0 1.0 0.0)
+    (GL11/glVertex2f -0.5 0.5)
+    (GL11/glColor3f 0.0 0.0 1.0)
+    (GL11/glVertex2f 0.5 0.5))
+  (GL11/glEnd))
 
-(defn main-loop [window width height]
+(defn main-loop [window]
   (while (not (GLFW/glfwWindowShouldClose window))
-    (update-globals)
-    (draw width height)
+    (draw)
     (GLFW/glfwSwapBuffers window)
     (GLFW/glfwPollEvents)))
 
 (defn -main []
+  (log/info (apply str (take 80 (repeat "*"))))
+  (log/info "Starting ....")
   (try
-    (let [{:keys [window width height]} (mk-window (:window @cfg))]
-      (init-gl width height)
-      (main-loop window width height)
-      #_(.free (:errorCallback @globals))
-      #_(.free (:keyCallback @globals))
+    (let [{:keys [window frame-buffer-width frame-buffer-height]} (init-window (:window @cfg))]
+      (init-gl frame-buffer-width frame-buffer-height)
+      (main-loop window)
       (GLFW/glfwDestroyWindow window))
     (finally
       (GLFW/glfwTerminate))))
